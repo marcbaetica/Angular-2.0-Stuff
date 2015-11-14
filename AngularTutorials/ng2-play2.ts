2e76bc66308a5cada1fbdd58db2e2c7f8e68950e
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-11-14 14:56:06 -0500

    js to html errFix

--- AngularTutorials/ng2-play2.ts
+++ AngularTutorials/ng2-play2.ts
@@ -8,13 +8,13 @@ import {Component, Template} from 'angular2/angular2';
 	inline: '
 		< div >
 			<label>Name:</label>
-			< input type= "text" #ref> //just need to reference the object to get its value updated
+			< input type= "text" #ref > <!-- just need to reference the object to get its value updated -->
 			<hr>
-			<h1>Hello {{ref.value }}!</h1>	// another update is that if it doesnt find the ref reference then angular will throw an error in the console (not try to create a model like in v1.X)
+			<h1>Hello {{ref.value }}!</h1>	<!-- another update is that if it doesnt find the ref reference then angular will throw an error in the console (not try to create a model like in v1.X) -->
 		</div>
 
-		//adding a button with to compensate for the lack of the event listener within the input tag
-		//the event listener for the button is located in the Hello class
+		< !-- adding a button with to compensate for the lack of the event listener within the input tag -->
+		< !-- the event listener for the button is located in the Hello class -->
 		<button id="button" (click)="onClick()"">Click me!</button>
 	'
 })
